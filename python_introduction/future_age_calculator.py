2a9511520e3920239672731f7291dc7258663fea
Enoch Nuertey <enochnuer111@gmail.com>
2024-06-29 20:31:24 +0000

    weather

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,4 +1,4 @@
 years_to_2050 = 27
 user_age = int(input("How old are you?"))
 age_in_2050 = years_to_2050 + user_age
-print("In 2050, you will be",age_in_2050,"years old.")
+print(f"In 2050, you will be {age_in_2050} years old.")
