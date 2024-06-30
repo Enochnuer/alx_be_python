2cb64fee3f9494f2b57c6f458e06ddee2457728e
Enoch Nuertey <enochnuer111@gmail.com>
2024-06-30 21:57:22 +0000

    pattern

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,7 +1,8 @@
 size_of_pattern = int(input("Enter the size of the pattern:"))
-while size_of_pattern >= 0:
-    for i in range(1,size_of_pattern + 1 ):
-        for j in range(1,size_of_pattern + 1):
-            print("*", end=" ")
-        print()
+i = 1
+while i <= size_of_pattern :
+    for j in range(1,size_of_pattern + 1):
+        print("*", end=" ")
+    print()
+    i += 1
      
